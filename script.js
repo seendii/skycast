c92b5f1c78259c2b9f6bbd5a465149eef03a9109
seendii <seendraa@gmail.com>
2026-06-19 10:14:46 +0000

    Added weather details and fallback location support

--- script.js
+++ script.js
@@ -1,61 +1,21 @@
-const API_KEY = "2a59234444ab7c0307eee217243be66b";
-
-async function getWeather(lat, lon) {
-
-    try {
-
-        const url =
-            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-
-        const response = await fetch(url);
-        const data = await response.json();
-
-        console.log("Weather Data:", data);
-
-        document.getElementById("city").innerText =
-            data.name;
-
-        document.getElementById("temperature").innerText =
-            Math.round(data.main.temp) + "°C";
-
-        document.getElementById("condition").innerText =
-            data.weather[0].description;
-
-        document.getElementById("weatherIcon").src =
-            `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-
-    } catch (error) {
-
-        console.error("Error:", error);
-
-        document.getElementById("condition").innerText =
-            "Failed to load weather";
-
-    }
-}
-
 if (navigator.geolocation) {
 
     navigator.geolocation.getCurrentPosition(
 
         function(position) {
 
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
-
-            console.log("Latitude:", lat);
-            console.log("Longitude:", lon);
-
-            getWeather(lat, lon);
+            getWeather(
+                position.coords.latitude,
+                position.coords.longitude
+            );
 
         },
 
         function(error) {
 
-            console.log(error);
+            console.log("Location denied. Using fallback location.");
 
-            document.getElementById("condition").innerText =
-                "Location access denied";
+            getWeather(10.1632, 76.6413);
 
         }
 
@@ -63,7 +23,6 @@ if (navigator.geolocation) {
 
 } else {
 
-    document.getElementById("condition").innerText =
-        "Geolocation not supported";
+    getWeather(10.1632, 76.6413);
 
 }
